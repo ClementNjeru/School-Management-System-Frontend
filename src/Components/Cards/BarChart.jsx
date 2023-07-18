bb90dd1e44e9fb0838367293a0a0f50b0db718fc
ClementNjeru <njeruclement36@gmail.com>
2023-07-18 10:59:07 +0300

    updates the BarChart

--- src/Components/Cards/BarChart.jsx
+++ src/Components/Cards/BarChart.jsx
@@ -1,11 +1,15 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { Box, IconButton, Pagination, Toolbar, Tooltip } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+// import axios from 'axios';
+
 
 
 
 const data = [
-  { name: 'Paid', value: 12 },
-  { name: 'Unpaid', value: 67},
+  { name: 'Paid', value: 40 },
+  { name: 'Unpaid', value: 7 },
   { name: 'Partial', value: 33 },
 ];
 
@@ -26,30 +30,57 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 
 function Chart() {
 
+  const [selectedGrade, setSelectedGrade] = useState();
+  const { data: classList } = useQuery(['classes-data'], {
+    cacheTime: 10 * 60 * 1000, // cache for 10 minutes
+  });
+  const handleChange = (e) => {
+    setSelectedGrade(e.target.value || null);
+  };
+
+
   return (
+    // dropdown box for selecting the class
     <div className="bg-white mt-3 lg:h-96 h-auto p-2 w-150 rounded-lg shadow">
       <p className="text-gray-700 font-semibold">Payment Status Per Class</p>
-      <>
 
-        <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-
-      </>
-
+      <Box>
+          <div className="flex justify-between">
+              <div className="flex">
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-3 pr-10 py-2 appearance-none dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   title = "Select Class"
+                   name='grade'
+                    id='grade'
+                    value={selectedGrade ?? ''}
+                    onChange={handleChange}
+                >
+                  
+                  <option value="">Select Class</option>
+                  {classList?.map((grade) => (
+                    <option key={grade?.id} value={grade?.id}>
+                      {grade?.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+      </Box>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey= "value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
     </div>
 
   );
