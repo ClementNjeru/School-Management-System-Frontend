aa2aa1e504f10bf53e82a886638c87c685a29b65
Ebenezr <ebenezarbukosia@gmail.com>
2023-07-28 16:08:50 +0300

    [REF] refactor dashboard component

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import {React, useState} from 'react';
+import { React } from 'react';
 import Stats from '../Components/Cards/Stats';
-import PaymentModesPie from '../Components/Cards/DataPie';
-// import FeeData from '../Components/Cards/FeeData';
-import BarChart  from '../Components/Cards/BarChart';
-import { Box, IconButton, Pagination, Toolbar, Tooltip } from '@mui/material';
+import PaymentModesPie from '../Components/Cards/PaymentModes';
 
+import BarChart from '../Components/Cards/PaymentsPie';
 
 const Dashboard = () => {
   const fetchData = async () => {
@@ -15,29 +13,22 @@ const Dashboard = () => {
         studentsResponce,
         teachersResponce,
         classesResponce,
-        guardiansResponce,
+
         paymentModeResponce,
-        feeResponce,
       ] = await Promise.all([
         axios.get(`${process.env.REACT_APP_BASE_URL}/students/count`),
         axios.get(`${process.env.REACT_APP_BASE_URL}/teachers/count`),
         axios.get(`${process.env.REACT_APP_BASE_URL}/classes/count`),
-        axios.get(`${process.env.REACT_APP_BASE_URL}/guardians/count`),
         axios.get(
           `${process.env.REACT_APP_BASE_URL}/payments/get/paymentmodes`
         ),
-        // axios.get(
-        //   `${process.env.REACT_APP_BASE_URL}/students/fee-status/${selectedGrade}}`
-        // ),
       ]);
 
       return {
         students: studentsResponce.data.totalStudents,
         teachers: teachersResponce.data.totalTeachers,
         classes: classesResponce.data.totalClasses,
-        guardians: guardiansResponce.data.totalGuardians,
         paymentModes: paymentModeResponce.data.todayRevenueByPaymentMode,
-        feePayment: feeResponce.data,
       };
     } catch (error) {
       throw new Error('Error fetching data');
@@ -49,29 +40,24 @@ const Dashboard = () => {
   const studentsCount = data?.students;
   const teachersCount = data?.teachers;
   const classesCount = data?.classes;
-  const guardiansCount = data?.guardians;
   const paymentModes = data?.paymentModes;
-  const feePayments = data?.feePayment;
+
+  console.log(studentsCount);
 
   return (
-    <section className="p-3 ">
+    <section className='p-3 '>
       <Stats
         studentsCount={studentsCount}
         teachersCount={teachersCount}
         classesCount={classesCount}
-        guardiansCount={guardiansCount}
         isLoading={isLoading}
       />
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 h-auto'>
         <PaymentModesPie paymentModes={paymentModes} isLoading={isLoading} />
-       
-        <BarChart isLoading={isLoading}/>
 
-  
+        <BarChart />
       </div>
-
-
     </section>
   );
 };
